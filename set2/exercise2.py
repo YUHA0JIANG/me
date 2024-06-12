9033c445cd2c16a881ac9c06d7aedba58683cd08
Yuhao Jiang <jiang3504453323@gmail.com>
2024-06-12 18:28:10 +1000

    update

--- set2/exercise2.py
+++ set2/exercise2.py
@@ -22,7 +22,7 @@ def set2exersise2(): # this is semi-right
     wordArray[0] = wordArray[0].upper()
     wordArray[1] = wordArray[1].upper() # <-- assigning to an array, another bracket problem
     wordArray[3] = wordArray[3].upper()
-    secret_word="".join(wordArray)
+    secret_word="".join(wordArray) #check
     print(secret_word)
     return secret_word
 
