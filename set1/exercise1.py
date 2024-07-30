f5a415ec5e309436290a94bcb6e28ce14006af2c
Yuhao Jiang <jiang3504453323@gmail.com>
2024-07-30 22:10:21 +1000

    1

--- set1/exercise1.py
+++ set1/exercise1.py
@@ -1,8 +1,1 @@
-import pandas as pd
-
-# 获取世界银行的金融普惠数据
-url = "http://api.worldbank.org/v2/en/indicator/IT.NET.USER.ZS?downloadformat=csv"
-data = pd.read_csv(url, skiprows=4)
-
-# 显示前几行数据
-print(data.head())
+print("Hello World")
